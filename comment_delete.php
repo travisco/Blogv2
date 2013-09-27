88a94f61384c0fb2fbcbb7c4a9bafb2c6c962d72
Travis <travisapotter@gmail.com>
2013-09-27 13:52:26 -0500

    not needed

--- comment_delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-    
-    incluse 'includes.php';
-    
-    mysql_safe_query('DELETE FROM comments WHERE id=%s LIMIT 1', $_GET['id']);
-    mysql_safe_query('UPDATE post SET num_comments-1 WHERE id=%s LIMIT 1', $_GET['id']);
-    redirect('post_view.php?id=' . $_GET['post']);
